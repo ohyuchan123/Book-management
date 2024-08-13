397a12dc890de36aa8ecef6e4bd00a4b9806240a
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-08-13 15:43:24 +0900

    :bug: ID 값이 한 번에 크게 늘어나는 문제를 해결

--- src/main/java/com/yse/dev/bookmanagement/dto/Member.java
+++ src/main/java/com/yse/dev/bookmanagement/dto/Member.java
@@ -2,6 +2,7 @@ package com.yse.dev.bookmanagement.dto;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,7 +12,7 @@ import lombok.Setter;
 @Setter
 public class Member {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 이렇게 함으로써 ID 값이 1씩 증가할
     private Long id;
     private String Username;
 }
