12fc12b65680e18b0bd773d204d7661dde6d5dd8
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-08-19 10:19:28 +0900

    revert commit

---
+++ src/main/java/com/yse/dev/bookmanagement/repository/MemberRepository.java
@@ -0,0 +1,23 @@
+package com.yse.dev.bookmanagement.repository;
+
+import com.yse.dev.bookmanagement.dto.Member;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Id;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepository {
+    @PersistenceContext //이 어노테이션은 EntitiyManager를 스프링 빈으로 등록해준다.
+    private EntityManager em;
+
+    // id를 반환해주는 이유는 CQS 이론 때문 -> 이 부분은 추후에 문서화 해서 작성할 예정
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member findById(Long id) {
+        return em.find(Member.class, id);
+    }
+}
