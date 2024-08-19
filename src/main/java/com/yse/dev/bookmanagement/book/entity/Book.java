ef9411d2ae2fba1e5186ec65da5923afb75275a1
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-08-19 15:34:03 +0900

    :memo: 첵 엔티티 주석 추가

--- src/main/java/com/yse/dev/bookmanagement/book/entity/Book.java
+++ src/main/java/com/yse/dev/bookmanagement/book/entity/Book.java
@@ -7,9 +7,14 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
-@Entity
-@Data
-@Builder
+
+// JPA는 데이터베이스의 테이블을 객체처럼 다루는 ORM(Object Relation Mapping) 기술 중 하나입니다.
+// 스프링 부트에서 JPA로 데이터를 다루기 위해 가장 먼저 하는 일은 엔티티 클래스를 만드는 것입니다.
+// 엔티티 클래스는 데이터베이스 테이블과 1 : 1로 매핑되는 자바 클래스입니다.
+
+@Entity // JPA가 해당 클래스를 엔티티로 인식하기 위해 사용하는 어노테이션
+@Data // Lombok을 사용하여 Getter, Setter, toString, equals, hashCode 등을 자동으로 생성하는 어노테이션
+@Builder // Lombok을 사용하여 Builder 패턴을 자동으로 생성하는 어노테이션
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
