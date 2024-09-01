859d3f4ef7eca0a1c2a4c48543a39c7dfa12ffd6
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-09-02 02:10:12 +0900

    :memo: 주석 추가

--- src/main/java/com/yse/dev/bookmanagement/book/dto/BookReadResponseDto.java
+++ src/main/java/com/yse/dev/bookmanagement/book/dto/BookReadResponseDto.java
@@ -7,6 +7,9 @@ import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @Getter
+// 엔터티를 직접 사용하지 않고 응답 객체를 따로 정의하는 이유는 HTTP 응답이 테이블 하나만 대상으로 하는 경우는
+// 거의 없기 때문입니다. 많은 경우 여러 엔터티에서 필요한 필드만 뽑아내서 조합시켜서 응답하기 때문에 응답별로 엔터티
+// 집합을 묶어주는 응답 DTO가 필요합니다.
 public class BookReadResponseDto {
     private Integer bookId;
     private String title;
