ede134c10406dab51a549a76b9c90c863212f164
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-09-03 09:47:04 +0900

    :sparkles: 책 정보를 읽기 위한 컨트롤러 코드 추가

--- src/main/java/com/yse/dev/bookmanagement/book/controller/BookController.java
+++ src/main/java/com/yse/dev/bookmanagement/book/controller/BookController.java
@@ -1,11 +1,17 @@
 package com.yse.dev.bookmanagement.book.controller;
 
 import com.yse.dev.bookmanagement.book.dto.BookCreateDTO;
+import com.yse.dev.bookmanagement.book.dto.BookReadResponseDto;
 import com.yse.dev.bookmanagement.book.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.NoSuchElementException;
 
 @Controller// 이 어노테이션에 있는 클래스는 스프링 부트가 브라우저의 요청을 받아들이는 컨트롤러라고 인지해서
 // 자바 빈으로 등록해서 관리하게 됩니다. 즉, 프레임워크에서 관리하는 클래스가 됩니다.
@@ -25,6 +31,24 @@ public class BookController {
     public String insert(BookCreateDTO bookCreateDTO){
         Integer bookId = this.bookService.insert(bookCreateDTO);
         return String.format("redirect:/book/read/%s",bookId);
+    }
+
+    @GetMapping("/book/read/{bookId}")
+    public ModelAndView read(@PathVariable Integer bookId){
+        ModelAndView modelAndView = new ModelAndView();
+
+        try {
+            BookReadResponseDto readResponseDto = this.bookService.read(bookId);
+            modelAndView.addObject("bookReadResponseDto", readResponseDto);
+            modelAndView.setViewName("book/read");
+        }catch (NoSuchElementException e){
+            modelAndView.setStatus(HttpStatus.UNPROCESSABLE_ENTITY);
+            modelAndView.addObject("message", "Book not found");
+            modelAndView.addObject("location","/book");
+            modelAndView.setViewName("common/error/422");
+        }
+
+        return modelAndView;
     }
 
 }
